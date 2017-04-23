40727f87f06dabd9b48097b785538e730574f3f2
Matryoshika <jonathancarlsson159@gmail.com>
2017-04-23 13:27:20 +0200

    fix syntax

--- src/main/java/se/Matryoshika/Echo/Client/Models/BakedMengerOneModel.java
+++ src/main/java/se/Matryoshika/Echo/Client/Models/BakedMengerOneModel.java
@@ -31,56 +31,51 @@ import se.Matryoshika.Echo.Common.Content.ContentRegistry;
 import se.Matryoshika.Echo.Common.Content.Blocks.CompressedBlock;
 import se.Matryoshika.Echo.Common.Utils.EchoConstants;
 
-public class BakedMengerOneModel implements IBakedModel{
-	
+public class BakedMengerOneModel implements IBakedModel {
+
 	public final static ModelResourceLocation TAG = new ModelResourceLocation("echo:compressed_block", "normal");
 
 	@Override
 	public List<BakedQuad> getQuads(IBlockState someState, EnumFacing side, long rand) {
-		
+
 		FMLLog.bigWarning("Not an IExtendedState", new Object[0]);
-		
-		if(!(someState instanceof IExtendedBlockState))
+
+		if (!(someState instanceof IExtendedBlockState))
 			return placeHolder(someState, side, rand);
-		
+
 		FMLLog.bigWarning("Yes it is", new Object[0]);
-		
+
 		IBakedModel wanted = Minecraft.getMinecraft().getBlockRendererDispatcher().getModelForState(Blocks.SNOW.getDefaultState());
 		IExtendedBlockState state = (IExtendedBlockState) someState;
 		IBlockState copy = state.getValue(CompressedBlock.IBS);
-		
-		
-        Minecraft mc = Minecraft.getMinecraft();
-        BlockRendererDispatcher blockRendererDispatcher = mc.getBlockRendererDispatcher();
-        BlockModelShapes blockModelShapes = blockRendererDispatcher.getBlockModelShapes();
-        IBakedModel copiedBlockModel = blockModelShapes.getModelForState(copy);
-        wanted = copiedBlockModel;
-		
-		
-		
+
+		Minecraft mc = Minecraft.getMinecraft();
+		BlockRendererDispatcher blockRendererDispatcher = mc.getBlockRendererDispatcher();
+		BlockModelShapes blockModelShapes = blockRendererDispatcher.getBlockModelShapes();
+		IBakedModel copiedBlockModel = blockModelShapes.getModelForState(copy);
+		wanted = copiedBlockModel;
+
 		List<BakedQuad> allQuads = new ArrayList<BakedQuad>();
-		
+
 		/*
-		for(float dx = -1; dx < 2; dx++){
-        	for(float dy = -1; dy < 2; dy++){
-        		for(float dz = -1; dz < 2; dz++){
-        			
-        			if((dx == 0 && dz == 0) || (dx == 0 && dy == 0) || (dy == 0 && dz == 0))
-        				continue;
-        			
-        			TSRSBakedMenger model = new TSRSBakedMenger(wanted, dx*(1/3f), dy*(1/3f), dz*(1/3f), 1/3f);
-        			allQuads.addAll(model.getQuads(state, side, rand));
-        		}
-        	}
-		}
-		*/
-		
+		 * for(float dx = -1; dx < 2; dx++){ for(float dy = -1; dy < 2; dy++){
+		 * for(float dz = -1; dz < 2; dz++){
+		 * 
+		 * if((dx == 0 && dz == 0) || (dx == 0 && dy == 0) || (dy == 0 && dz ==
+		 * 0)) continue;
+		 * 
+		 * TSRSBakedMenger model = new TSRSBakedMenger(wanted, dx*(1/3f),
+		 * dy*(1/3f), dz*(1/3f), 1/3f); allQuads.addAll(model.getQuads(state,
+		 * side, rand)); } } }
+		 */
+
 		allQuads.addAll(wanted.getQuads(copy, side, rand));
 		return allQuads;
 	}
-	
+
 	public List<BakedQuad> placeHolder(IBlockState someState, EnumFacing side, long rand) {
-		return Minecraft.getMinecraft().getBlockRendererDispatcher().getModelForState(Blocks.COBBLESTONE.getDefaultState()).getQuads(someState, side, rand);
+		return Minecraft.getMinecraft().getBlockRendererDispatcher()
+				.getModelForState(Blocks.COBBLESTONE.getDefaultState()).getQuads(someState, side, rand);
 	}
 
 	@Override
@@ -112,16 +107,19 @@ public class BakedMengerOneModel implements IBakedModel{
 	public ItemOverrideList getOverrides() {
 		return itemHandler;
 	}
-	
+
 	private final ItemOverrideList itemHandler = new ItemOverrideList(ImmutableList.of()) {
 		@Nonnull
 		@Override
-		public IBakedModel handleItemState(@Nonnull IBakedModel model, ItemStack stack, World world, EntityLivingBase entity) {
-			
-			if(!stack.hasTagCompound())
-				return Minecraft.getMinecraft().getBlockRendererDispatcher().getModelForState(Blocks.COBBLESTONE.getDefaultState());
-			
-			IBlockState state = NBTUtil.func_190008_d(stack.getTagCompound().getCompoundTag(EchoConstants.NBT_BLOCKSTATE));
+		public IBakedModel handleItemState(@Nonnull IBakedModel model, ItemStack stack, World world,
+				EntityLivingBase entity) {
+
+			if (!stack.hasTagCompound())
+				return Minecraft.getMinecraft().getBlockRendererDispatcher()
+						.getModelForState(Blocks.COBBLESTONE.getDefaultState());
+
+			IBlockState state = NBTUtil
+					.func_190008_d(stack.getTagCompound().getCompoundTag(EchoConstants.NBT_BLOCKSTATE));
 			return Minecraft.getMinecraft().getBlockRendererDispatcher().getModelForState(state);
 		}
 	};
