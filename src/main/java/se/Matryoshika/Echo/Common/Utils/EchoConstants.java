f83dbafa9a1efb268f1af97544a457261f4b4b1a
Matryoshika <jonathancarlsson159@gmail.com>
2017-04-24 18:58:17 +0200

    New constants

--- src/main/java/se/Matryoshika/Echo/Common/Utils/EchoConstants.java
+++ src/main/java/se/Matryoshika/Echo/Common/Utils/EchoConstants.java
@@ -4,5 +4,7 @@ public class EchoConstants {
 	
 	public static String NBT_BLOCKSTATE = "echo:blockstate";
 	public static String NBT_TIER = "echo:tier";
+	public static String NBT_INVENTORY = "echo:inventory";
+	public static String NBT_DISPLAY_NAME = "echo:display_name";
 
 }
