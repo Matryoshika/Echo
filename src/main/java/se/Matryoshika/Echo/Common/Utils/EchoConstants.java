573ba72918f5580c4f05828d601e626e57c35207
Matryoshika <jonathancarlsson159@gmail.com>
2017-04-26 23:06:56 +0200

    Some more of them constants

--- src/main/java/se/Matryoshika/Echo/Common/Utils/EchoConstants.java
+++ src/main/java/se/Matryoshika/Echo/Common/Utils/EchoConstants.java
@@ -2,9 +2,11 @@ package se.Matryoshika.Echo.Common.Utils;
 
 public class EchoConstants {
 	
-	public static String NBT_BLOCKSTATE = "echo:blockstate";
-	public static String NBT_TIER = "echo:tier";
-	public static String NBT_INVENTORY = "echo:inventory";
-	public static String NBT_DISPLAY_NAME = "echo:display_name";
+	public static final String NBT_BLOCKSTATE = "echo:blockstate";
+	public static final String NBT_TIER = "echo:tier";
+	public static final String NBT_INVENTORY = "echo:inventory";
+	public static final String NBT_DISPLAY_NAME = "echo:display_name";
+	
+	public static final int GUI_PHASE_SUBSTANTIATOR = 0;
 
 }
