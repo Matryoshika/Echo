e67380086a265eb149857ef4ac5dd82d9944709c
Matryoshika <jonathancarlsson159@gmail.com>
2017-04-29 01:21:06 +0200

    More constants

--- src/main/java/se/Matryoshika/Echo/Common/Utils/EchoConstants.java
+++ src/main/java/se/Matryoshika/Echo/Common/Utils/EchoConstants.java
@@ -5,6 +5,8 @@ public class EchoConstants {
 	public static final String NBT_BLOCKSTATE = "echo:blockstate";
 	public static final String NBT_TIER = "echo:tier";
 	public static final String NBT_FABRICATORS = "echo:placed_fabricators";
+	public static final String NBT_PHASE_INPUT = "echo:phase_inputs";
+	public static final String NBT_PHASE_OUTPUT = "echo:phase_outputs";
 	
 	public static final int GUI_PHASE_SUBSTANTIATOR = 0;
 
