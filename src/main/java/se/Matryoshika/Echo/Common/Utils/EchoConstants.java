6adae13778b8462441638b9764a58e39e86ea8e1
Matryoshika <jonathancarlsson159@gmail.com>
2017-05-06 03:48:26 +0200

    Constants Constantly Constellating Constitutional Constellations

--- src/main/java/se/Matryoshika/Echo/Common/Utils/EchoConstants.java
+++ src/main/java/se/Matryoshika/Echo/Common/Utils/EchoConstants.java
@@ -9,6 +9,10 @@ public class EchoConstants {
 	public static final String NBT_PHASE_OUTPUT = "echo:phase_outputs";
 	public static final String NBT_TD_CLASS = "echo:td_entity_class";
 	public static final String NBT_TD_KILL = "echo:td_player_kill";
+	public static final String NBT_TD_META = "echo:td_meta";
+	public static final String NBT_TD_REDSTONE = "echo:td_redstone";
+	public static final String NBT_TD_BEHEADING = "echo:td_beheading";
+	public static final String NBT_TD_LOOTING = "echo:td_looting";
 	
 	public static final int GUI_PHASE_SUBSTANTIATOR = 0;
 
