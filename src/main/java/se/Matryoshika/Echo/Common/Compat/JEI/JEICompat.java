c28ed61f6c5ba0722e6bb873bd7eec4eb9950e07
Matryoshika <jonathancarlsson159@gmail.com>
2017-05-11 14:13:52 +0200

    Cleaning up JEI compat

--- src/main/java/se/Matryoshika/Echo/Common/Compat/JEI/JEICompat.java
+++ src/main/java/se/Matryoshika/Echo/Common/Compat/JEI/JEICompat.java
@@ -1,10 +1,5 @@
 package se.Matryoshika.Echo.Common.Compat.JEI;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.google.common.collect.ImmutableList;
-
 import mezz.jei.api.IJeiHelpers;
 import mezz.jei.api.IJeiRuntime;
 import mezz.jei.api.IModPlugin;
@@ -12,20 +7,9 @@ import mezz.jei.api.IModRegistry;
 import mezz.jei.api.ISubtypeRegistry;
 import mezz.jei.api.JEIPlugin;
 import mezz.jei.api.ingredients.IModIngredientRegistration;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTUtil;
-import se.Matryoshika.Echo.Common.Compat.Botania.BotaniaCompat;
 import se.Matryoshika.Echo.Common.Content.ContentRegistry;
-import se.Matryoshika.Echo.Common.Content.Recipes.IRecipeLaniaiteFabricator;
-import se.Matryoshika.Echo.Common.Content.Recipes.IRecipeLaniaitePool;
-import se.Matryoshika.Echo.Common.Content.Recipes.IRecipeLaniaiteSpreader;
-import se.Matryoshika.Echo.Common.Content.Recipes.IRecipePhaseSubstantiator;
-import se.Matryoshika.Echo.Common.Utils.BlockStateJSON;
-import se.Matryoshika.Echo.Common.Utils.EchoConstants;
 
 @JEIPlugin
 public class JEICompat implements IModPlugin {
