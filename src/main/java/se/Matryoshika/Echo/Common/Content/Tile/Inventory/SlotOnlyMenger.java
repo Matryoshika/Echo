f6a1f0baad117c96adcd9d5c842d08b17eda0c93
Matryoshika <jonathancarlsson159@gmail.com>
2017-05-14 06:14:34 +0200

    Fix the Phase Substantiator's Menger slots accepting wrong tier in wrong slot

--- src/main/java/se/Matryoshika/Echo/Common/Content/Tile/Inventory/SlotOnlyMenger.java
+++ src/main/java/se/Matryoshika/Echo/Common/Content/Tile/Inventory/SlotOnlyMenger.java
@@ -5,15 +5,19 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.SlotItemHandler;
 import se.Matryoshika.Echo.Common.Content.ContentRegistry;
+import se.Matryoshika.Echo.Common.Utils.EchoConstants;
 
 public class SlotOnlyMenger extends SlotItemHandler {
 	
-	public SlotOnlyMenger(IItemHandler itemHandler, int index, int xPosition, int yPosition) {
+	public byte tier;
+	
+	public SlotOnlyMenger(IItemHandler itemHandler, int index, int xPosition, int yPosition, byte tier) {
 		super(itemHandler, index, xPosition, yPosition);
+		this.tier = tier;
 	}
 
 	@Override
 	public boolean isItemValid(ItemStack stack) {
-		return super.isItemValid(stack) && stack.getItem() == Item.getItemFromBlock(ContentRegistry.COMPRESSED_BLOCK);
+		return super.isItemValid(stack) && stack.getItem() == Item.getItemFromBlock(ContentRegistry.COMPRESSED_BLOCK) && stack.getTagCompound().getByte(EchoConstants.NBT_TIER) == tier;
 	}
 }
