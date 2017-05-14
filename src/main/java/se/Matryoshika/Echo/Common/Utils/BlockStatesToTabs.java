4f8ad8012718cf8e730deb898def26fbf57ac38a
Matryoshika <jonathancarlsson159@gmail.com>
2017-05-14 06:20:52 +0200

    Adding util for getting all ores

--- src/main/java/se/Matryoshika/Echo/Common/Utils/BlockStatesToTabs.java
+++ src/main/java/se/Matryoshika/Echo/Common/Utils/BlockStatesToTabs.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockOre;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
@@ -15,22 +16,34 @@ public class BlockStatesToTabs {
 	public static List<IBlockState> states = new ArrayList<IBlockState>();
 	public static final ArrayList<Block> blacklist = new ArrayList<Block>(Arrays.asList(new Block[]{ContentRegistry.VOID, ContentRegistry.COMPRESSED_BLOCK}));
 	
+	public static List<IBlockState> ores = new ArrayList<IBlockState>();
 	
 	public static void init(){
 		BlockFlag:
 			for(Block block : ForgeRegistries.BLOCKS){
-				/*ArrayList<IBlockState> sstates = new ArrayList<IBlockState>(Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelShapes().getBlockStateMapper().getVariants(block).keySet());
-				for(IBlockState state : sstates){
-					if(!state.isFullBlock())
-						continue;
-						*/
+				List<IBlockState> used = new ArrayList<>();
 				for(Block nope : blacklist)
 					if(block.getRegistryName().toString().equals(nope.getRegistryName().toString()))
 						continue BlockFlag;
 				
-				if(block.getDefaultState().isFullBlock() && block.getBlockLayer() == BlockRenderLayer.SOLID)
-					states.add(block.getDefaultState());
-				
+				for(IBlockState state : block.getBlockState().getValidStates()){
+					if(state.isFullBlock() && block.getBlockLayer() == BlockRenderLayer.SOLID && !used.contains(state)){
+						states.add(state);
+						used.add(state);
+						if(block instanceof BlockOre)
+							ores.add(state);
+					}
+				}
 			}
+	}
+	
+	
+	public static List<IBlockState> getStates(){
+		
+		if(!states.isEmpty())
+			return states;
+		
+		init();
+		return states;
 	}
 }
